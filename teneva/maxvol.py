e98c5819a48f3ae8d6f4138fa213454284280e92
Andrei Chertkov <andrei.chertkov@skolkovotech.ru>
2021-02-07 12:39:52 +0300

    Rewrite the maxvol algorithm (the most compact code)

--- teneva/maxvol.py
+++ teneva/maxvol.py
@@ -3,46 +3,22 @@ import scipy as sp
 from scipy.linalg import get_lapack_funcs, get_blas_funcs
 
 
-def maxvol(A, tol=1.05, max_iters=100, top_k_index=-1):
-    tol = max(1., tol)
+def maxvol(A, e=1.05, K=100):
     N, r = A.shape
-    if N <= r:
-        return np.arange(N, dtype=np.int32), np.eye(N, dtype=A.dtype)
-    if top_k_index == -1 or top_k_index > N:
-        top_k_index = N
-    if top_k_index < r:
-        top_k_index = r
-
-    B = np.copy(A[:top_k_index], order='F')
-    C = np.copy(A.T, order='F')
-    H, ipiv, info = get_lapack_funcs('getrf', [B])(B, overwrite_a=1)
-
-    index = np.arange(N, dtype=np.int32)
-    for i in range(r):
-        tmp = index[i]
-        index[i] = index[ipiv[i]]
-        index[ipiv[i]] = tmp
-
-    B = H[:r]
-    trtrs = get_lapack_funcs('trtrs', [B])
-    trtrs(B, C, trans=1, lower=0, unitdiag=0, overwrite_b=1)
-    trtrs(B, C, trans=1, lower=1, unitdiag=1, overwrite_b=1)
-
-    i, j = divmod(abs(C[:,:top_k_index]).argmax(), top_k_index)
-    ger = get_blas_funcs('ger', [C])
-    iters = 0
-    
-    while abs(C[i,j]) > tol and iters < max_iters:
-        index[i] = j
-        tmp_row = C[i].copy()
-        tmp_column = C[:,j].copy()
-        tmp_column[i] -= 1.
-        alpha = -1./C[i,j]
-        ger(alpha, tmp_column, tmp_row, a=C, overwrite_a=1)
-        iters += 1
-        i, j = divmod(abs(C[:,:top_k_index]).argmax(), top_k_index)
-
-    return index[:r].copy(), C.T
+    assert e >= 1 and N > r and K > 0
+    P, L, U = lu(A)
+    I = P.argmax(axis=0)
+    Q = solve_triangular(U, A.T, trans=1, lower=False)
+    B = solve_triangular(L[:r, :], Q, trans=1, unit_diagonal=True, lower=True)
+    for _ in range(K):
+        i, j = np.divmod(np.abs(B).argmax(), N)
+        if np.abs(B[i, j]) <= e: break
+        x = B[i, :]
+        y = B[:, j].copy()
+        y[i]-= 1.
+        I[i] = j
+        B-= np.outer(y / B[i, j], x)
+    return I[:r], B.T
 
 
 def rect_maxvol(A, tol=1., maxK=None, min_add_K=None, minK=None,
