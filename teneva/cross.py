4ed5b2f832d13c975024b7552132406a6aaeeac0
AndreiChertkov <a.chertkov@skoltech.ru>
2021-11-30 17:37:59 +0300

    Rewrite cross.py code

--- teneva/cross.py
+++ teneva/cross.py
@@ -3,122 +3,159 @@ import numpy as np
 
 from .maxvol import maxvol
 from .maxvol import rect_maxvol
-from .utils import kron
-from .utils import reshape
-
-
-def _index_merge(i1, i2, i3):
-    r1 = i1.shape[0] or 1
-    r2 = i2.shape[0]
-    r3 = i3.shape[0] or 1
-    w1 = kron(np.ones((r3 * r2, 1), dtype=np.int32), i1)
-    w2 = np.empty((w1.shape[0], 0))
-    if i3.size and r2:
-        w2 = kron(i3, np.ones((r1 * r2, 1), dtype=np.int32))
-    w3 = kron(kron(np.ones((r3, 1), dtype=np.int32), i2), np.ones((r1, 1), dtype=np.int32))
-    return np.hstack((w1, w3, w2))
-
-
-def _index_stack(n, r, Il, Ir, right=True):
-    e1 = np.ones((r, 1), dtype=np.int32)
-    e2 = np.ones((n, 1), dtype=np.int32)
-    e3 = reshape(np.arange(n, dtype=np.int32), (-1, 1))
-    if right:
-        J = kron(e1, e3)
-        if Ir.size:
-            J = np.hstack((J, kron(Ir, e2)))
-    else:
-        J = kron(e3, e1)
-        if Il.size:
-            J = np.hstack((kron(e2, Il), J))
-    return reshape(J, (n * r, -1))
-
-
-def _qr_maxvol(G, R0, I0=None, right=True):
-    r1, n, r2 = G.shape
-    if right:
-        G = np.tensordot(G, R0, 1)
-        r = G.shape[2]
-        r2 = r
-        G = reshape(G, (G.shape[0], -1)).T
-    else:
-        G = np.tensordot(R0, G, 1)
-        r = G.shape[0]
-        G = reshape(G, (-1, G.shape[-1]))
-
-    Q, R = np.linalg.qr(G)
-    I, B = maxvol(Q)
-    G = reshape(B.T, (-1, n, r2))
-    J = _index_stack(n, r, I0, I0, right)[I, :]
-    R = np.dot(Q[I, :], R)
-    if right:
-        R = R.T
-    return G, J, R
-
-
-def _update_core(G, Il, Ir, fun, kickrank, rf, tau=1.1, right=True):
-    r1, n, r2 = G.shape
-    p = reshape(np.arange(n, dtype=np.int32), (-1, 1))
-    G = fun(_index_merge(Il, p, Ir))
-
-    if right:
-        r = r2
-        G = reshape(G, (r1, -1)).T
-    else:
-        r = r1
-        G = reshape(G, (-1, r2))
-
-    q, s1, v1 = np.linalg.svd(G, full_matrices=False)
-    R = np.diag(s1) @ v1
-
-    N, u = q.shape
-    N_min = min(N, u + kickrank)
-    N_max = min(N, u + kickrank + rf)
-    if N <= u:
-        I = np.arange(q.shape[0], dtype=np.int32)
-        B = np.eye(q.shape[0], dtype=q.dtype)
-    else:
-        I, B = rect_maxvol(q, tau, N_min, N_max)
-
-    if right:
-        G = reshape(B.T, (-1, n, r2))
-    else:
-        G = reshape(B, (r1, n, -1))
-
-    R = q[I, :].dot(R)
-
-    J = _index_stack(n, r, Il, Ir, right)[I, :]
-
-    if right:
-        R = R.T
-    return G, J, R
 
 
 def cross(f, Y0, nswp=10, kickrank=2, rf=2):
     d = len(Y0)
     Y = [Y0[i].copy() for i in range(d)]
-    Il = [np.empty((1, 0), dtype=np.int32)] + [None for i in range(d)]
-    Ir = [None for i in range(d)] + [np.empty((1, 0), dtype=np.int32)]
+    Il = [np.empty((1, 0), dtype=int)] + [None for i in range(d)]
+    Ir = [None for i in range(d)] + [np.empty((1, 0), dtype=int)]
+    Ig = [_reshape(np.arange(G.shape[1], dtype=int), (-1, 1)) for G in Y]
 
     R = np.ones((1, 1))
     for i in range(d-1):
-        Y[i], Il[i+1], R = _qr_maxvol(Y[i], R, Il[i], right=False)
+        Y[i], Il[i+1], R = cross_prep_l2r(Y[i], R, Il[i])
 
     R = np.ones((1, 1))
     for i in range(d-1, 0, -1):
-        Y[i], Ir[i], R = _qr_maxvol(Y[i], R, Ir[i+1], right=True)
+        Y[i], Ir[i], R = cross_prep_r2l(Y[i], R, Ir[i+1])
 
     for _ in range(nswp):
         R = np.ones((1, 1))
         for i in range(d):
-            Y[i] = np.tensordot(R, Y[i], 1)
-            Y[i], Il[i+1], R = _update_core(Y[i], Il[i], Ir[i+1], f, kickrank, rf, right=False)
+            G = np.tensordot(R, Y[i], 1)
+            y = f(cross_index_merge(Il[i], Ig[i], Ir[i+1]))
+            Y[i], Il[i+1], R = cross_build_l2r(*G.shape, y, Il[i], kickrank, rf)
         Y[d-1] = np.tensordot(Y[d-1], R, 1)
 
         R = np.ones((1, 1))
         for i in range(d-1, -1, -1):
-            Y[i] = np.tensordot(Y[i], R, 1)
-            Y[i], Ir[i], R = _update_core(Y[i], Il[i], Ir[i+1], f, kickrank, rf, right=True)
+            G = np.tensordot(Y[i], R, 1)
+            y = f(cross_index_merge(Il[i], Ig[i], Ir[i+1]))
+            Y[i], Ir[i], R = cross_build_r2l(*G.shape, y, Ir[i+1], kickrank, rf)
         Y[0] = np.tensordot(R, Y[0], 1)
 
     return Y
+
+
+def cross_build_l2r(r1, n, r2, y, I, kickrank, rf):
+    G = _reshape(y, (-1, r2))
+    Q, s, V = _svd(G)
+    Ind, B = _maxvol_rect(Q, kickrank, rf)
+    G = _reshape(B, (r1, n, -1))
+    J = cross_index_stack_l2r(n, r1, I)[Ind, :]
+    R = Q[Ind, :] @ np.diag(s) @ V
+    return G, J, R
+
+
+def cross_build_r2l(r1, n, r2, y, I, kickrank, rf):
+    G = _reshape(y, (r1, -1)).T
+    Q, s, V = _svd(G)
+    Ind, B = _maxvol_rect(Q, kickrank, rf)
+    G = _reshape(B.T, (-1, n, r2))
+    J = cross_index_stack_r2l(n, r2, I)[Ind, :]
+    R = (Q[Ind, :] @ np.diag(s) @ V).T
+    return G, J, R
+
+
+def cross_index_merge(i1, i2, i3):
+    r1 = i1.shape[0] or 1
+    r2 = i2.shape[0]
+    r3 = i3.shape[0] or 1
+    w1 = _kron(_ones(r3 * r2), i1)
+    w2 = np.empty((w1.shape[0], 0))
+    if i3.size and r2:
+        w2 = _kron(i3, _ones(r1 * r2))
+    w3 = _kron(_kron(_ones(r3), i2), _ones(r1))
+    return np.hstack((w1, w3, w2))
+
+
+def cross_index_stack_l2r(n, r, I):
+    e1 = _ones(r)
+    e2 = _ones(n)
+    e3 = _reshape(np.arange(n, dtype=int), (-1, 1))
+    J = _kron(e3, e1)
+    if I.size:
+        J = np.hstack((_kron(e2, I), J))
+    return _reshape(J, (n * r, -1))
+
+
+def cross_index_stack_r2l(n, r, I):
+    e1 = _ones(r)
+    e2 = _ones(n)
+    e3 = _reshape(np.arange(n, dtype=int), (-1, 1))
+    J = _kron(e1, e3)
+    if I.size:
+        J = np.hstack((J, _kron(I, e2)))
+    return _reshape(J, (n * r, -1))
+
+
+def cross_prep_l2r(G, R0, I0=None):
+    r1, n, r2 = G.shape
+    G = np.tensordot(R0, G, 1)
+    r = G.shape[0]
+    G = _reshape(G, (-1, G.shape[-1]))
+
+    Q, R = _qr(G)
+    I, B = _maxvol(Q)
+    G = _reshape(B.T, (-1, n, r2))
+    J = cross_index_stack_l2r(n, r, I0)[I, :]
+    R = Q[I, :] @ R
+    return G, J, R
+
+
+def cross_prep_r2l(G, R0, I0=None):
+    r1, n, r2 = G.shape
+    G = np.tensordot(G, R0, 1)
+    r = G.shape[2]
+    r2 = r
+    G = _reshape(G, (G.shape[0], -1)).T
+
+    Q, R = _qr(G)
+    I, B = _maxvol(Q)
+    G = _reshape(B.T, (-1, n, r2))
+    J = cross_index_stack_r2l(n, r, I0)[I, :]
+    R =(Q[I, :] @ R).T
+    return G, J, R
+
+
+def _kron(a, b):
+    return np.kron(a, b)
+
+
+def _maxvol(a):
+    return maxvol(a)
+
+
+def _maxvol_rect(a, kickrank=1, rf=1, tau=1.1):
+    N, u = a.shape
+    N_min = min(N, u + kickrank)
+    N_max = min(N, u + kickrank + rf)
+
+    if N <= u:
+        I = np.arange(N, dtype=int)
+        B = np.eye(N, dtype=a.dtype)
+    else:
+        I, B = rect_maxvol(a, tau, N_min, N_max)
+
+    return I, B
+
+
+def _ones(k, m=1):
+    return np.ones((k, m), dtype=int)
+
+
+def _qr(a):
+    return np.linalg.qr(a)
+
+
+def _reshape(a, shape):
+    return np.reshape(a, shape, order='F')
+
+
+def _svd(a, full_matrices=False):
+    try:
+        return np.linalg.svd(a, full_matrices=full_matrices)
+    except:
+        b = a + 1.E-14 * np.max(np.abs(a)) * np.random.randn(*a.shape)
+        return np.linalg.svd(b, full_matrices=full_matrices)
