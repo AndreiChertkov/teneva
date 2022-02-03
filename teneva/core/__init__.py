8b7d6c41b71038d8f9537a009e7ae7131714e852
AndreiChertkov <a.chertkov@skoltech.ru>
2022-02-03 17:19:22 +0300

    Move all main modules into the "core" folder

---
+++ teneva/core/__init__.py
@@ -0,0 +1,64 @@
+from .als import als
+from .als import als2
+
+
+from .anova import ANOVA
+from .anova import anova
+
+
+from .cheb import cheb_bld
+from .cheb import cheb_get
+from .cheb import cheb_get_full
+from .cheb import cheb_int
+from .cheb import cheb_pol
+from .cheb import cheb_sum
+
+
+from .cross import cross
+
+
+from .grid import grid_flat
+from .grid import grid_prep_opts
+from .grid import ind2poi
+from .grid import ind2str
+from .grid import sample_lhs
+from .grid import sample_tt
+from .grid import str2ind
+
+
+from .maxvol import maxvol
+from .maxvol import maxvol_rect
+
+
+from .stat import cdf_confidence
+from .stat import cdf_getter
+
+
+from .svd import matrix_skeleton
+from .svd import matrix_svd
+from .svd import svd
+from .svd import svd_incomplete
+
+
+from .tensor import accuracy
+from .tensor import add
+from .tensor import add_many
+from .tensor import const
+from .tensor import copy
+from .tensor import erank
+from .tensor import full
+from .tensor import get
+from .tensor import getter
+from .tensor import mean
+from .tensor import mul
+from .tensor import mul_scalar
+from .tensor import norm
+from .tensor import orthogonalize
+from .tensor import rand
+from .tensor import ranks
+from .tensor import shape
+from .tensor import show
+from .tensor import size
+from .tensor import sub
+from .tensor import sum
+from .tensor import truncate
