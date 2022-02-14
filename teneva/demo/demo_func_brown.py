cd5ff3affacfce4ba640071286f1fa99ae07f192
AndreiChertkov <a.chertkov@skoltech.ru>
2022-02-14 14:03:30 +0300

    Add Brown and Schaffer demo functions

---
+++ teneva/demo/demo_func_brown.py
@@ -0,0 +1,39 @@
+"""Package teneva, module demo.demo_func_brown: function.
+
+This module contains class that implements analytical Brown function
+for demo and tests.
+
+"""
+
+import numpy as np
+
+
+from .demo_func import DemoFunc
+
+
+class DemoFuncBrown(DemoFunc):
+    def __init__(self, d):
+        """Brown function for demo and tests.
+
+        Args:
+            d (int): number of dimensions.
+
+        Note:
+            See Momin Jamil, Xin-She Yang. "A literature survey of benchmark
+            functions for global optimization problems". Journal of
+            Mathematical Modelling and Numerical Optimisation 2013; 4:150-194
+            for details.
+
+        """
+        super().__init__(d, 'Brown')
+
+        self.set_lim(-1., +4.)
+        self.set_min([0.]*self.d, 0.)
+
+    def _calc(self, x):
+        y = x[:-1]**(2*(x[1:]**2+1)) + x[1:]**(2*(x[:-1]**2+1))
+        return np.sum(y)
+
+    def _comp(self, X):
+        Y = X[:, :-1]**(2*(X[:, 1:]**2+1)) + X[:, 1:]**(2*(X[:, :-1]**2+1))
+        return np.sum(Y, axis=1)
---
+++ teneva/demo/demo_func_schaffer.py
@@ -0,0 +1,41 @@
+"""Package teneva, module demo.demo_func_schaffer: function.
+
+This module contains class that implements analytical Schaffer function
+for demo and tests.
+
+"""
+
+import numpy as np
+
+
+from .demo_func import DemoFunc
+
+
+class DemoFuncSchaffer(DemoFunc):
+    def __init__(self, d):
+        """Schaffer function for demo and tests.
+
+        Args:
+            d (int): number of dimensions.
+
+        Note:
+            See Momin Jamil, Xin-She Yang. "A literature survey of benchmark
+            functions for global optimization problems". Journal of
+            Mathematical Modelling and Numerical Optimisation 2013; 4:150-194
+            for details.
+
+        """
+        super().__init__(d, 'Schaffer')
+
+        self.set_lim(-100., +100.)
+        self.set_min([0.]*self.d, 0.)
+
+    def _calc(self, x):
+        z = x[:-1]^2 + x[1:]^2
+        y = 0.5 + (np.sin(np.sqrt(z))**2 - 0.5) / (1. + 0.001 * z)**2
+        return np.sum(y)
+
+    def _comp(self, X):
+        Z = X[:, :-1]^2 + X[:, 1:]^2
+        Y = 0.5 + (np.sin(np.sqrt(Z))**2 - 0.5) / (1. + 0.001 * Z)**2
+        return np.sum(Y, axis=1)
