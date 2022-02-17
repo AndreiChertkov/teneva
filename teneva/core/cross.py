94be94bad9df66ad3e6a5beff3951d471508306e
AndreiChertkov <a.chertkov@skoltech.ru>
2022-02-17 18:51:28 +0300

    Big update for cross (CAM) code

--- teneva/core/cross.py
+++ teneva/core/cross.py
@@ -82,64 +82,52 @@ def cross(f, Y0, e, evals=None, nswp=10, dr_min=1, dr_max=2, cache=None,
     R = np.ones((1, 1))
     for i in range(d-1):
         G = np.tensordot(R, Y[i], 1)
-        Y[i], Il[i+1], R = cross_prep_l2r(G, Ig[i], Il[i])
+        Y[i], R, Il[i+1] = _iter(G, Ig[i], Il[i], l2r=True)
 
     R = np.ones((1, 1))
     for i in range(d-1, 0, -1):
         G = np.tensordot(Y[i], R, 1)
-        Y[i], Ir[i], R = cross_prep_r2l(G, Ig[i], Ir[i+1])
+        Y[i], R, Ir[i] = _iter(G, Ig[i], Ir[i+1], l2r=False)
 
+    info['evals_max'] = evals
     info['k_evals'] = 0
     info['k_cache'] = 0
     info['e'] = -1.
     info['nswp'] = 0
     info['stop'] = None
 
-    def func(I):
-        if cache is None:
-            if evals is not None and info['k_evals'] + len(I) > evals:
-                return None
-            info['k_evals'] += len(I)
-            return f(I)
-
-        I_new = np.array([i for i in I if ind2str(i) not in cache])
-        if len(I_new):
-            if evals is not None and info['k_evals'] + len(I_new) > evals:
-                return None
-            Y_new = f(I_new)
-            for k, i in enumerate(I_new):
-                cache[ind2str(i)] = Y_new[k]
-
-        info['k_cache'] += len(I) - len(I_new)
-        info['k_evals'] += len(I_new)
-
-        Y = np.array([cache[ind2str(i)] for i in I])
-        return Y
-
     Yold = None
     for swp in range(nswp):
         R = np.ones((1, 1))
         for i in range(d):
             G = np.tensordot(R, Y[i], 1)
-            y = func(cross_index_merge(Il[i], Ig[i], Ir[i+1]))
+            I_curr = _index_merge(Il[i], Ig[i], Ir[i+1])
+            y = _func(f, I_curr, cache, info)
+
             if y is None:
                 Y[i] = G
                 info['stop'] = 'evals'
                 return truncate(Y, e)
-            Y[i], Il[i+1], R = cross_build_l2r(
-                *G.shape, y, Ig[i], Il[i], dr_min, dr_max)
+
+            G = _reshape(y, (-1, G.shape[1], G.shape[2]))
+            Y[i], R, Il[i+1] = _iter(G, Ig[i], Il[i], dr_min, dr_max, l2r=True)
+
         Y[d-1] = np.tensordot(Y[d-1], R, 1)
 
         R = np.ones((1, 1))
         for i in range(d-1, -1, -1):
             G = np.tensordot(Y[i], R, 1)
-            y = func(cross_index_merge(Il[i], Ig[i], Ir[i+1]))
+            I_curr = _index_merge(Il[i], Ig[i], Ir[i+1])
+            y = _func(f, I_curr, cache, info)
+
             if y is None:
                 Y[i] = G
                 info['stop'] = 'evals'
                 return truncate(Y, e)
-            Y[i], Ir[i], R = cross_build_r2l(
-                *G.shape, y, Ig[i], Ir[i+1], dr_min, dr_max)
+
+            G = _reshape(y, (-1, G.shape[1], G.shape[2]))
+            Y[i], R, Ir[i] = _iter(G, Ig[i], Ir[i+1], dr_min, dr_max, l2r=False)
+
         Y[0] = np.tensordot(R, Y[0], 1)
 
         info['nswp'] = swp + 1
@@ -158,126 +146,124 @@ def cross(f, Y0, e, evals=None, nswp=10, dr_min=1, dr_max=2, cache=None,
     return truncate(Y, e)
 
 
-def cross_build_l2r(r1, n, r2, y, Ig, I, dr_min, dr_max):
-    G = _reshape(y, (-1, r2))
-    Q, s, V = _svd(G)
-    Ind, B = _maxvol_rect(Q, dr_min, dr_max)
-    G = _reshape(B, (r1, n, -1))
-    J = cross_index_stack_l2r(r1, Ig, I)[Ind, :]
-    R = Q[Ind, :] @ np.diag(s) @ V
-    return G, J, R
-
-
-def cross_build_r2l(r1, n, r2, y, Ig, I, dr_min, dr_max):
-    G = _reshape(y, (r1, -1)).T
-    Q, s, V = _svd(G)
-    Ind, B = _maxvol_rect(Q, dr_min, dr_max)
-    G = _reshape(B.T, (-1, n, r2))
-    J = cross_index_stack_r2l(r2, Ig, I)[Ind, :]
-    R = (Q[Ind, :] @ np.diag(s) @ V).T
-    return G, J, R
-
-
 def cross_cache2data(cache):
     I = np.array([str2ind(s) for s in cache.keys()], dtype=int)
     Y = np.array([y for y in cache.values()])
     return I, Y
 
 
-def cross_index_merge(i1, i2, i3):
+def _func(f, I, cache=None, info={}):
+    if cache is None:
+        if info['evals_max'] and info['k_evals'] + len(I) > info['evals_max']:
+            return None
+        info['k_evals'] += len(I)
+        return f(I)
+
+    I_new = np.array([i for i in I if ind2str(i) not in cache])
+    if len(I_new):
+        if info['evals_max'] and info['k_evals']+len(I_new) > info['evals_max']:
+            return None
+        Y_new = f(I_new)
+        for k, i in enumerate(I_new):
+            cache[ind2str(i)] = Y_new[k]
+
+    info['k_cache'] += len(I) - len(I_new)
+    info['k_evals'] += len(I_new)
+
+    Y = np.array([cache[ind2str(i)] for i in I])
+    return Y
+
+
+def _index_merge(i1, i2, i3):
     r1 = i1.shape[0] or 1
     r2 = i2.shape[0]
     r3 = i3.shape[0] or 1
 
-    w1 = _kron(_ones(r3 * r2), i1)
+    w1 = np.kron(_ones(r3 * r2), i1)
 
-    w2 = _kron(_kron(_ones(r3), i2), _ones(r1))
+    w2 = np.kron(np.kron(_ones(r3), i2), _ones(r1))
 
     if i3.size and r2:
-        w3 = _kron(i3, _ones(r1 * r2))
+        w3 = np.kron(i3, _ones(r1 * r2))
     else:
         w3 = np.empty((w1.shape[0], 0))
 
     return np.hstack((w1, w2, w3))
 
 
-def cross_index_stack_l2r(r, Ig, I):
+def _index_stack(r, Ig, I, l2r=True):
     n = Ig.size
-    J = _kron(Ig, _ones(r))
-    if I.size:
-        J = np.hstack((_kron(_ones(n), I), J))
-    return _reshape(J, (n * r, -1))
+    I_new = np.kron(Ig, _ones(r)) if l2r else np.kron(_ones(r), Ig)
+    if I.size and l2r:
+        I_new = np.hstack((np.kron(_ones(n), I), I_new))
+    if I.size and not l2r:
+        I_new = np.hstack((I_new, np.kron(I, _ones(n))))
+    return _reshape(I_new, (n * r, -1))
 
 
-def cross_index_stack_r2l(r, Ig, I):
-    n = Ig.size
-    J = _kron(_ones(r), Ig)
-    if I.size:
-        J = np.hstack((J, _kron(I, _ones(n))))
-    return _reshape(J, (n * r, -1))
+def _iter(G, Ig, I, dr_min=0, dr_max=0, l2r=True):
+    """Find important rows in unfolding matrix from the given TT-core.
 
+    Args:
+        G (np.ndarray): TT-core of the shape r1 x n x r2 that relates to the
+            k-th mode of the tensor (k = 1, 2, ..., d).
+        Ig (np.ndarray): full range of grid indices for the k-th mode, i.e.
+            it is just [0, 1, ..., n-1].
+        I (np.ndarray): list of selected multiindices for the previous (k-1)-th
+            mode. It has the shape r1 x (k-1), and each its row
+            (i_1, ..., i_{k-1}) corresponds to the one selected row in the
+            undolding matrix. For the 0-th mode it is should be empty array.
+        dr_min (int): minimum number of added rows in the process of adaptively
+            increasing the TT-rank of the approximation using the algorithm
+            maxvol_rect (see teneva.core.maxvol.maxvol_rect for more details).
+            Note that "dr_min" should be no bigger than "dr_max".
+        dr_max (int): maximum number of added rows in the process of adaptively
+            increasing the TT-rank of the approximation using the algorithm
+            maxvol_rect (see teneva.core.maxvol.maxvol_rect for more details).
+            Note that "dr_max" should be no less than "dr_min".
+        l2r (bool): ...
 
-def cross_prep_l2r(G, Ig, I=None):
+    Returns:
+        np.ndarray: updated TT-core.
+        np.ndarray: list of selected multiindices for the current k-th mode.
+            It has the shape r2 x k, and each its row (i_1, ..., i_k)
+            corresponds to the one selected row in the undolding matrix.
+        np.ndarray: the matrix of the shape r2 x r2 by which the next TT-core
+            should be multiplied to keep the tensor unchanged.
+
+    """
     r1, n, r2 = G.shape
-    G = _reshape(G, (r1 * n, r2))
+    G = _reshape(G, (r1 * n, r2)) if l2r else _reshape(G, (r1, n * r2)).T
 
-    Q, R = _qr(G)
-    Ind, B = _maxvol(Q)
+    if dr_max > 0:
+        Q, s, V = np.linalg.svd(G, full_matrices=False)
+        R = np.diag(s) @ V
 
-    G = _reshape(B.T, (-1, n, r2))
-    J = cross_index_stack_l2r(r1, Ig, I)[Ind, :]
-    R = Q[Ind, :] @ R
-    return G, J, R
-
-
-def cross_prep_r2l(G, Ig, I=None):
-    r1, n, r2 = G.shape
-    G = _reshape(G, (r1, n * r2)).T
-
-    Q, R = _qr(G)
-    Ind, B = _maxvol(Q)
-
-    G = _reshape(B.T, (-1, n, r2))
-    J = cross_index_stack_r2l(r2, Ig, I)[Ind, :]
-    R = (Q[Ind, :] @ R).T
-    return G, J, R
-
-
-def _kron(a, b):
-    return np.kron(a, b)
-
-
-def _maxvol(a):
-    return maxvol(a)
-
-
-def _maxvol_rect(a, dr_min=1, dr_max=2, tau=1.1):
-    N, u = a.shape
-
-    if N <= u:
-        I = np.arange(N, dtype=int)
-        B = np.eye(N, dtype=a.dtype)
+        n_rows, n_cols = Q.shape
+        if n_rows <= n_cols:
+            ind, B = np.arange(n_rows, dtype=int), np.eye(n_rows, dtype=a.dtype)
+        else:
+            tau = 1.1
+            ind, B = maxvol_rect(Q, 1.1, dr_min, dr_max)
     else:
-        I, B = maxvol_rect(a, tau, dr_min, dr_max)
+        Q, R = np.linalg.qr(G)
 
-    return I, B
+        ind, B = maxvol(Q)
+
+    G = B if l2r else B.T
+    G = _reshape(G, (r1, n, -1)) if l2r else _reshape(G, (-1, n, r2))
+
+    R = Q[ind, :] @ R
+    R = R if l2r else R.T
+
+    I_new = _index_stack(r1 if l2r else r2, Ig, I, l2r)[ind, :]
+
+    return G, R, I_new
 
 
 def _ones(k, m=1):
     return np.ones((k, m), dtype=int)
 
 
-def _qr(a):
-    return np.linalg.qr(a)
-
-
 def _reshape(a, shape):
     return np.reshape(a, shape, order='F')
-
-
-def _svd(a, full_matrices=False):
-    try:
-        return np.linalg.svd(a, full_matrices=full_matrices)
-    except:
-        b = a + 1.E-14 * np.max(np.abs(a)) * np.random.randn(*a.shape)
-        return np.linalg.svd(b, full_matrices=full_matrices)
