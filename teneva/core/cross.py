98d1d0e2927eeb5298e89c1d633ecb724a3573a5
AndreiChertkov <a.chertkov@skoltech.ru>
2022-03-14 23:54:02 +0300

    Huge update for module "core/cross.py"

--- teneva/core/cross.py
+++ teneva/core/cross.py
@@ -2,257 +2,191 @@
 
 This module contains the function "cross" which computes the TT-approximation
 for implicit tensor given functionally by multidimensional cross approximation
-method in the TT-format (TT-CAM).
+method in the TT-format (TT-CROSS).
 
 """
 import numpy as np
 
 
 from .grid import ind_to_str
-from .grid import str_to_ind
 from .maxvol import maxvol
 from .maxvol import maxvol_rect
 from .tensor import accuracy
 from .tensor import copy
-from .tensor import truncate
+from .tensor import shape
+from .transformation import truncate
 
 
-def cross(f, Y0, e, evals=None, nswp=10, dr_min=1, dr_max=2, cache=None,
-    info={}):
+def cross(f, Y0, e, m=None, nswp=10, dr_min=1, dr_max=2, info={}, cache=None):
     """Compute the TT-approximation for implicit tensor given functionally.
 
     Args:
-        f (function): function f(I) which computes tensor elements for a given
-            set of multi-indices I, where I is 2D np.ndarray of the shape
-            [samples x dimensions]. The function should return 1D np.ndarray of
-            the length equals to samples.
+        f (function): function f(I) which computes tensor elements for the given
+            set of multi-indices I, where I is a 2D np.ndarray of the shape
+            [samples, dimensions]. The function should return 1D np.ndarray of
+            the length equals to "samples".
         Y0 (list): TT-tensor, which is the initial approximation for algorithm.
         e (float): accuracy (> 0) for truncation of the final result and
             algorithm convergence criterion. If between iterations the relative
             rate of solution change is less than this value, then the operation
             of the algorithm will be interrupted.
-        evals (int): an optionally set limit on the maximum number of requests
-            to the objective function. If specified, then the total number of
+        m (int): an optionally set limit on the maximum number of requests to
+            the objective function. If specified, then the total number of
             requests will not exceed this value. Note that the actual number of
             requests may be less, since the values are requested in batches.
         nswp (int): maximum number of iterations (sweeps) of the algorithm.
         dr_min (int): minimum number of added rows in the process of adaptively
             increasing the TT-rank of the approximation using the algorithm
-            maxvol_rect (see teneva.core.maxvol.maxvol_rect for more details).
-            Note that "dr_min" should be no bigger than "dr_max".
+            maxvol_rect (see "maxvol_rect" function for more details). Note
+            that "dr_min" should be no bigger than "dr_max".
         dr_max (int): maximum number of added rows in the process of adaptively
             increasing the TT-rank of the approximation using the algorithm
-            maxvol_rect (see teneva.core.maxvol.maxvol_rect for more details).
-            Note that "dr_max" should be no less than "dr_min".
-        cache (dict): an optionally set dictionary, which will be filled with
-            requested function values. Since the algorithm sometimes requests
-            the same tensor indices, the use of such a cache in some cases can
-            speed up the operation of the algorithm if the time to find a value
-            in the cache is less than the time to calculate the function.
+            maxvol_rect (see "maxvol_rect" function for more details). Note
+            that "dr_max" should be no less than "dr_min".
         info (dict): an optionally set dictionary, which will be filled with
             reference information about the process of the algorithm operation.
-            At the end of the function work, it will contain parameters:
-            "k_evals" - total number of requests to target function "f";
-            "k_cache" - total number of requests to cache;
-            "e" - the final value of the convergence criterion;
-            "nswp" - the real number of performed iterations (sweeps);
-            "stop" - stop type of the algorithm (see note below).
+            At the end of the function work, it will contain parameters: "e" -
+            the final value of the convergence criterion; "m" - total number of
+            requests to target function "f"; "m_cache" - total number of
+            requests to cache; "nswp" - the real number of performed iterations
+            (sweeps); "stop" - stop type of the algorithm (see note below).
+        cache (dict): an optionally set dictionary, which will be filled with
+            requested function values. Since the algorithm sometimes requests
+            the same tensor indices, the use of such a cache may speed up the
+            operation of the algorithm if the time to find a value in the cache
+            is less than the time to calculate the function.
 
     Returns:
         list: TT-Tensor which approximates the implicit tensor.
 
     Note:
         Note that the end of the algorithm operation occurs when one of the
-        three criteria is reached: 1) the maximum number of iterations ("nswp")
-        performed; 2) the maximum allowable number of the objective function
-        calls ("evals") has been done (more precisely, if the next request will
-        result in exceeding this value, then algorithm will not perform this
-        new request); 3) the convergence criterion ("e") is reached. The
-        corresponding stop type ("nswp", "evals" or "e") will be written into
-        the item "stop" of the "info" dictionary.
+        following criteria is reached: 1) the maximum number of iterations
+        ("nswp") performed; 2) the maximum allowable number of the objective
+        function calls ("m") has been done (more precisely, if the next request
+        will result in exceeding this value, then algorithm will not perform
+        this new request). If "m" is set, then other criteria will be ignored;
+        3) the convergence criterion ("e") is reached; 4) the algorithm is
+        already converged (all requested values are in the cache already). The
+        related stop type ("nswp", "m", "e", "conv") will be written into the
+        item "stop" of the "info" dictionary.
 
     """
-    info['evals_max'] = evals
-    info['k_evals'] = 0
-    info['k_cache'] = 0
     info['e'] = -1.
+    info['m'] = 0
+    info['m_cache'] = 0
+    info['m_max'] = int(m) if m else None
     info['nswp'] = 0
     info['stop'] = None
 
     Y = copy(Y0)
     d = len(Y)
+    n = shape(Y)
 
-    Il = [np.empty((1, 0), dtype=int)] + [None for i in range(d)]
-    Ir = [None for i in range(d)] + [np.empty((1, 0), dtype=int)]
-    Ig = [_reshape(np.arange(G.shape[1], dtype=int), (-1, 1)) for G in Y]
+    Ig = [_reshape(np.arange(k, dtype=int), (-1, 1)) for k in n] # Grid
+    Ir = [None for i in range(d+1)]                              # Row indices
+    Ic = [None for i in range(d+1)]                              # Col indices
 
     R = np.ones((1, 1))
     for i in range(d):
         G = np.tensordot(R, Y[i], 1)
-        Y[i], R, Il[i+1] = _iter(G, Ig[i], Il[i], l2r=True)
+        Y[i], R, Ir[i+1] = _iter(G, Ig[i], Ir[i], l2r=True)
     Y[d-1] = np.tensordot(Y[d-1], R, 1)
 
     R = np.ones((1, 1))
     for i in range(d-1, -1, -1):
         G = np.tensordot(Y[i], R, 1)
-        Y[i], R, Ir[i] = _iter(G, Ig[i], Ir[i+1], l2r=False)
+        Y[i], R, Ic[i] = _iter(G, Ig[i], Ic[i+1], l2r=False)
     Y[0] = np.tensordot(R, Y[0], 1)
 
     Yold = None
-    for swp in range(nswp):
+    while True:
         R = np.ones((1, 1))
         for i in range(d):
-            G = np.tensordot(R, Y[i], 1)
-
-            I_curr = _index_merge(Il[i], Ig[i], Ir[i+1])
-            y = _func(f, I_curr, cache, info)
-
-            if y is None:
-                Y[i] = G
-                info['stop'] = 'evals'
+            Z = _func(f, Ig[i], Ir[i], Ic[i+1], info, cache)
+            if Z is None:
+                info['stop'] = 'm'
+                Y[i] = np.tensordot(R, Y[i], 1)
                 return truncate(Y, e)
-
-            G = _reshape(y, G.shape)
-            Y[i], R, Il[i+1] = _iter(G, Ig[i], Il[i], dr_min, dr_max, l2r=True)
-
+            Y[i], R, Ir[i+1] = _iter(Z, Ig[i], Ir[i], dr_min, dr_max, l2r=True)
         Y[d-1] = np.tensordot(Y[d-1], R, 1)
 
         R = np.ones((1, 1))
         for i in range(d-1, -1, -1):
-            G = np.tensordot(Y[i], R, 1)
-
-            I_curr = _index_merge(Il[i], Ig[i], Ir[i+1])
-            y = _func(f, I_curr, cache, info)
-
-            if y is None:
-                Y[i] = G
-                info['stop'] = 'evals'
+            Z = _func(f, Ig[i], Ir[i], Ic[i+1], info, cache)
+            if Z is None:
+                info['stop'] = 'm'
+                Y[i] = np.tensordot(Y[i], R, 1)
                 return truncate(Y, e)
-
-            G = _reshape(y, G.shape)
-            Y[i], R, Ir[i] = _iter(G, Ig[i], Ir[i+1], dr_min, dr_max, l2r=False)
-
+            Y[i], R, Ic[i] = _iter(Z, Ig[i], Ic[i+1], dr_min, dr_max, l2r=False)
         Y[0] = np.tensordot(R, Y[0], 1)
-
-        info['nswp'] = swp + 1
 
         if Yold is not None:
             eps = accuracy(Y, Yold)
             info['e'] = eps
 
-            if eps <= e:
+            if not info['m_max'] and eps <= e:
                 info['stop'] = 'e'
                 return truncate(Y, e)
 
+        info['nswp'] += 1
+        if not info['m_max'] and info['nswp'] >= nswp:
+            info['stop'] = 'nswp'
+            return truncate(Y, e)
+
+        if info['m_cache'] > 5 * info['m']:
+            info['stop'] = 'conv'
+            return truncate(Y, e)
+
         Yold = copy(Y)
 
-    info['stop'] = 'nswp'
-    return truncate(Y, e)
+
+def _func(f, Ig, Ir, Ic, info, cache=None):
+    n = Ig.shape[0]
+    r1 = Ir.shape[0] if Ir is not None else 1
+    r2 = Ic.shape[0] if Ic is not None else 1
+
+    I = np.kron(np.kron(_ones(r2), Ig), _ones(r1))
+    if Ir is not None:
+        Ir_ = np.kron(_ones(n * r2), Ir)
+        I = np.hstack((Ir_, I))
+    if Ic is not None:
+        Ic_ = np.kron(Ic, _ones(r1 * n))
+        I = np.hstack((I, Ic_))
+
+    y = _func_eval(f, I, info, cache)
+    if y is not None:
+        return _reshape(y, (r1, n, r2))
 
 
-def cross_cache2data(cache):
-    I = np.array([str_to_ind(s) for s in cache.keys()], dtype=int)
-    Y = np.array([y for y in cache.values()])
-    return I, Y
-
-
-def _func(f, I, cache=None, info={}):
+def _func_eval(f, I, info, cache=None):
     if cache is None:
-        if info['evals_max'] and info['k_evals'] + len(I) > info['evals_max']:
+        if info['m_max'] and info['m'] + len(I) > info['m_max']:
             return None
-        info['k_evals'] += len(I)
+        info['m'] += len(I)
         return f(I)
 
     I_new = np.array([i for i in I if ind_to_str(i) not in cache])
     if len(I_new):
-        if info['evals_max'] and info['k_evals']+len(I_new) > info['evals_max']:
+        if info['m_max'] and info['m'] + len(I_new) > info['m_max']:
             return None
         Y_new = f(I_new)
         for k, i in enumerate(I_new):
             cache[ind_to_str(i)] = Y_new[k]
 
-    info['k_cache'] += len(I) - len(I_new)
-    info['k_evals'] += len(I_new)
+    info['m'] += len(I_new)
+    info['m_cache'] += len(I) - len(I_new)
 
-    Y = np.array([cache[ind_to_str(i)] for i in I])
-    return Y
-
-
-def _index_merge(i1, i2, i3):
-    r1 = i1.shape[0] or 1
-    r2 = i2.shape[0]
-    r3 = i3.shape[0] or 1
-
-    w1 = np.kron(_ones(r3 * r2), i1)
-
-    w2 = np.kron(np.kron(_ones(r3), i2), _ones(r1))
-
-    if i3.size and r2:
-        w3 = np.kron(i3, _ones(r1 * r2))
-    else:
-        w3 = np.empty((w1.shape[0], 0))
-
-    return np.hstack((w1, w2, w3))
+    return np.array([cache[ind_to_str(i)] for i in I])
 
 
-def _index_stack(r, Ig, I, l2r=True):
-    n = Ig.size
-    I_new = np.kron(Ig, _ones(r)) if l2r else np.kron(_ones(r), Ig)
-    if I.size and l2r:
-        I_new = np.hstack((np.kron(_ones(n), I), I_new))
-    if I.size and not l2r:
-        I_new = np.hstack((I_new, np.kron(I, _ones(n))))
-    return _reshape(I_new, (n * r, -1))
+def _iter(Z, Ig, I=None, dr_min=0, dr_max=0, l2r=True):
+    r1, n, r2 = Z.shape
+    r = r1 if l2r else r2
+    Z = _reshape(Z, (r1 * n, r2)) if l2r else _reshape(Z, (r1, n * r2)).T
 
-
-def _iter(G, Ig, I, dr_min=0, dr_max=0, l2r=True):
-    """Find important rows in unfolding matrix from the given TT-core.
-
-    Args:
-        G (np.ndarray): TT-core of the shape r1 x n x r2 that relates to the
-            k-th mode of the tensor (k = 1, 2, ..., d).
-        Ig (np.ndarray): full range of grid indices for the k-th mode, i.e.
-            it is just [0, 1, ..., n-1].
-        I (np.ndarray): list of selected multiindices for the previous (k-1)-th
-            mode. It has the shape r1 x (k-1), and each its row
-            (i_1, ..., i_{k-1}) corresponds to the one selected row in the
-            undolding matrix. For the 0-th mode it is should be empty array.
-        dr_min (int): minimum number of added rows in the process of adaptively
-            increasing the TT-rank of the approximation using the algorithm
-            maxvol_rect (see teneva.core.maxvol.maxvol_rect for more details).
-            Note that "dr_min" should be no bigger than "dr_max".
-        dr_max (int): maximum number of added rows in the process of adaptively
-            increasing the TT-rank of the approximation using the algorithm
-            maxvol_rect (see teneva.core.maxvol.maxvol_rect for more details).
-            Note that "dr_max" should be no less than "dr_min".
-        l2r (bool): ...
-
-    Returns:
-        np.ndarray: updated TT-core.
-        np.ndarray: list of selected multiindices for the current k-th mode.
-            It has the shape r2 x k, and each its row (i_1, ..., i_k)
-            corresponds to the one selected row in the undolding matrix.
-        np.ndarray: the matrix of the shape r2 x r2 by which the next TT-core
-            should be multiplied to keep the tensor unchanged.
-
-    """
-    r1, n, r2 = G.shape
-    G = _reshape(G, (r1 * n, r2)) if l2r else _reshape(G, (r1, n * r2)).T
-
-    if dr_max > 0:
-        Q, s, V = np.linalg.svd(G, full_matrices=False)
-        R = np.diag(s) @ V
-
-        n_rows, n_cols = Q.shape
-        if n_rows <= n_cols:
-            ind, B = np.arange(n_rows, dtype=int), np.eye(n_rows, dtype=a.dtype)
-        else:
-            tau = 1.1
-            ind, B = maxvol_rect(Q, 1.1, dr_min, dr_max)
-    else:
-        Q, R = np.linalg.qr(G)
-
-        ind, B = maxvol(Q)
+    Q, R = np.linalg.qr(Z)
+    ind, B = _maxvol(Q, dr_min, dr_max)
 
     G = B if l2r else B.T
     G = _reshape(G, (r1, n, -1)) if l2r else _reshape(G, (-1, n, r2))
@@ -260,14 +194,34 @@ def _iter(G, Ig, I, dr_min=0, dr_max=0, l2r=True):
     R = Q[ind, :] @ R
     R = R if l2r else R.T
 
-    I_new = _index_stack(r1 if l2r else r2, Ig, I, l2r)[ind, :]
+    I_new = np.kron(Ig, _ones(r)) if l2r else np.kron(_ones(r), Ig)
+    if I is not None:
+        I_old = np.kron(_ones(n), I) if l2r else np.kron(I, _ones(n))
+        I_new = np.hstack((I_old, I_new)) if l2r else np.hstack((I_new, I_old))
+    I_new = I_new[ind, :]
 
     return G, R, I_new
+
+
+def _maxvol(A, dr_min=0, dr_max=0, tau=1.1):
+    n, r = A.shape
+    dr_max = min(dr_max, n - r)
+    dr_min = min(dr_min, dr_max)
+
+    if n <= r:
+        I = np.arange(n, dtype=int)
+        B = np.eye(n, dtype=float)
+    elif dr_max == 0:
+        I, B = maxvol(A)
+    else:
+        I, B = maxvol_rect(A, tau, dr_min, dr_max)
+
+    return I, B
 
 
 def _ones(k, m=1):
     return np.ones((k, m), dtype=int)
 
 
-def _reshape(a, shape):
-    return np.reshape(a, shape, order='F')
+def _reshape(A, shape):
+    return np.reshape(A, shape, order='F')
