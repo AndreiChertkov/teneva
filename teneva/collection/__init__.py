9e18deccbbb9e0b59fa6c1ab65c6ec82280f274d
AndreiChertkov <a.chertkov@skoltech.ru>
2022-07-11 20:26:08 +0300

    Add "tensor_poly" function and related demo

--- teneva/collection/__init__.py
+++ teneva/collection/__init__.py
@@ -3,6 +3,7 @@ from .matrices import matrix_delta
 
 from .tensors import tensor_const
 from .tensors import tensor_delta
+from .tensors import tensor_poly
 
 
 from .vectors import vector_delta
--- teneva/collection/tensors.py
+++ teneva/collection/tensors.py
@@ -1,10 +1,13 @@
 """Package teneva, module collection.tensors: various useful TT-tensors.
 
 This module contains the collection of functions for explicit construction of
-various useful TT-tensors (delta function and others).
+various useful TT-tensors (delta function, polynomial and others).
 
 """
 import numpy as np
+
+
+import teneva
 
 
 def tensor_const(n, v=1.):
@@ -44,4 +47,51 @@ def tensor_delta(n, ind, v=1.):
         G[0, ind[i], 0] = 1.
         Y.append(G)
     Y[-1][0, ind[-1], 0] = v
+    return Y
+
+
+def tensor_poly(n, shift=0., power=2, scale=1.):
+    """Build TT-tensor that is polynomial like scale * (index + shift)^power.
+
+    Args:
+        n (list, np.ndarray): shape of the tensor. It should be list or
+            np.ndarray of the length "d", where "d" is a number of dimensions.
+        shift (float, list, np.ndarray): the shift value. It should be list or
+            np.ndarray of the length "d". It may be also float value.
+        power (int): the power of polynomial.
+        scale (float): the scale.
+
+    Returns:
+        list: TT-tensor.
+
+    """
+    d = len(n)
+    Y = []
+
+    shift = teneva.grid_prep_opt(shift, d)
+
+    def _get(m, j):
+        return (m + shift[j])**power
+
+    for j, k in enumerate(n):
+        if j == 0:
+            G = np.zeros((1, k, 2))
+            for m in range(k):
+                G[0, m, :] = np.array([1., _get(m, j)])
+
+        if j > 0 and j < d-1:
+            G = np.zeros((2, k, 2))
+            for m in range(k):
+                G[:, m, :] = np.array([
+                    [1., _get(m, j)],
+                    [0., 1.],
+                ])
+
+        if j == d - 1:
+            G = np.zeros((2, k, 1))
+            for m in range(k):
+                G[:, m, 0] = np.array([_get(m, j) * scale, scale])
+
+        Y.append(G)
+
     return Y
