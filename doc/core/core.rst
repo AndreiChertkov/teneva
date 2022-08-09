68ff5b55263c23c93ebaab42d010ff08f73429f9
AndreiChertkov <a.chertkov@skoltech.ru>
2022-08-09 16:42:57 +0300

    Fixes for functions and demo in "core" module

--- doc/core/core.rst
+++ doc/core/core.rst
@@ -14,7 +14,43 @@ Module core: operations with individual TT-cores
 
   .. code-block:: python
 
-    # TODO
+    r = 4                            # Left TT-rank
+    n = 10                           # Mode size
+    q = 5                            # Right TT-rank
+    G = np.random.randn(r, n, q)     # Create random TT-core
+    
+    Q, p = teneva.core_stab(G)       # Perform scaling
+    
+    print(p)
+    print(np.max(np.abs(Q)))
+    print(np.max(np.abs(G - 2**p * Q)))
+
+    # >>> ----------------------------------------
+    # >>> Output:
+
+    # 1
+    # 1.3600845832948094
+    # 0.0
+    # 
+
+  For convenience, we can set an initial value for the power-factor:
+
+  .. code-block:: python
+
+    p0 = 2
+    Q, p = teneva.core_stab(G, p0)
+    
+    print(p)
+    print(np.max(np.abs(Q)))
+    print(np.max(np.abs(G - 2**(p-p0) * Q)))
+
+    # >>> ----------------------------------------
+    # >>> Output:
+
+    # 3
+    # 1.3600845832948094
+    # 0.0
+    # 
 
 
 .. autofunction:: teneva.core_tt_to_qtt
@@ -23,6 +59,42 @@ Module core: operations with individual TT-cores
 
   .. code-block:: python
 
-    # TODO
+    r = 4                            # Left TT-rank
+    n = 2**10                        # Mode size
+    q = 5                            # Right TT-rank
+    G = np.random.randn(r, n, q)     # Create random TT-core
+    
+    Y = teneva.core_tt_to_qtt(G)     # Transform the core to QTT
+    
+    teneva.show(Y)
+
+    # >>> ----------------------------------------
+    # >>> Output:
+
+    #    2   2   2   2   2   2   2   2   2   2  
+    #   / \ / \ / \ / \ / \ / \ / \ / \ / \ / \ 
+    #  1   8   16  32  64 128  80  40  20  10  5  
+    # 
+    # 
+
+  .. code-block:: python
+
+    for G in Y:
+        print(G.shape)
+
+    # >>> ----------------------------------------
+    # >>> Output:
+
+    # (4, 2, 8)
+    # (8, 2, 16)
+    # (16, 2, 32)
+    # (32, 2, 64)
+    # (64, 2, 128)
+    # (128, 2, 80)
+    # (80, 2, 40)
+    # (40, 2, 20)
+    # (20, 2, 10)
+    # (10, 2, 5)
+    # 
 
 
--- teneva/core/core.py
+++ teneva/core/core.py
@@ -7,10 +7,25 @@ import numpy as np
 
 
 from .svd import matrix_svd
+from .utils import _reshape
 
 
 def core_stab(G, p0=0, thr=1.E-100):
-    # TODO: add docstring and add into demo
+    """Scaling for the passed TT-core, i.e., G -> (Q, p), G = 2^p * Q.
+
+    Args:
+        G (np.ndarray): TT-core in the form of 3-dimensional array.
+        p0 (int): optional initial value of the power-factor (it will be added
+            to returned value "p").
+        thr (float): threshold value for applying scaling (if the maximum
+            modulo element in the TT-core is less than this value, then scaling
+            will not be performed).
+
+    Returns:
+        (np.ndarray, int): scaled TT-core (Q) and power-factor (p), such that
+            G = 2^p * Q.
+
+    """
     v_max = np.max(np.abs(G))
 
     if v_max <= thr:
@@ -22,25 +37,37 @@ def core_stab(G, p0=0, thr=1.E-100):
     return Q, p0 + p
 
 
-def core_tt_to_qtt(core, e=0, r_max=int(1e12)):
-    # TODO: add docstring and add into demo
-    r1, n, r2 = core.shape
+def core_tt_to_qtt(G, e=0., r=1.E+12):
+    """Transform the TT-core into a list of QTT-cores (TODO!).
+
+    Args:
+        G (np.ndarray): TT-core in the form of 3-dimensional array of the shape
+            r1 x n x r2. The mode size should be a power of two, i.e., n=2^d.
+        e (float): desired approximation accuracy (> 0).
+        r (int, float): maximum rank for the SVD decomposition (> 0).
+
+    Returns:
+        (list of np.ndarray): list of QTT-cores of the shape [q1, 2, q2], which
+            approximates the given TT-core "G".
+
+    """
+    r1, n, r2 = G.shape
     d = int(np.log2(n))
-    assert 2**d == n
+    if 2**d != n:
+        raise ValueError('Invalid mode size (it should be a power of two)')
 
-    A = core.reshape(-1, r2, order='F')
-    A, V0 = matrix_svd(A, e=e, r=r_max)
+    A = _reshape(G, (-1, r2))
+    A, V0 = matrix_svd(A, e, r)
 
-    res = []
+    Y = []
     for i in range(d-1):
         As = A.shape[0] // 2
-        r = A.shape[1]
+        q = A.shape[1]
         A = np.hstack([A[:As], A[As:]])
-        A, V = matrix_svd(A, e=e, r=r_max)
-        res.append(V.reshape(-1, 2, r, order='C'))
+        A, V = matrix_svd(A, e, r)
+        Y.append(_reshape(V, (-1, 2, q), order='C'))
 
+    Y.append(_reshape(A, (r1, 2, -1)))
+    Y[0] = np.einsum("ijk,kl", Y[0], V0)
 
-    res.append(A.reshape(r1, 2, -1, order='F'))
-    res[0] = np.einsum("ijk,kl", res[0], V0)
-
-    return res[::-1]
+    return Y[::-1]
