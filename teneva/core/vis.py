265b19dd2009f86e57c6fa2ed6d3d396f9f3f914
AndreiChertkov <andre.chertkov@gmail.com>
2022-09-04 16:45:31 +0300

    Update "vis" function

--- teneva/core/vis.py
+++ teneva/core/vis.py
@@ -14,15 +14,13 @@ def show(Y):
         Y (list): TT-tensor.
 
     """
+    d = len(Y)
     n = teneva.shape(Y)
     r = teneva.ranks(Y)
-    k = int(np.ceil(np.log10(max(n)+1))) + 1
-    l = max(int(np.ceil(np.log10(max(r)+1))) + 1, k)
-    form_str = '{:^' + str(l) + '}'
 
-    s0 = ' '*(l//2)
-    s1 = s0 + ''.join([form_str.format(k) for k in n])
-    s2 = s0 + ''.join([form_str.format('/ \\') for _ in n])
-    s3 = ''.join([form_str.format(q) for q in r])
-
-    print(f'{s1}\n{s2}\n{s3}\n')
+    text = f'TT-{d}D : '
+    for k in range(d):
+        text += f'|{n[k]}|'
+        if k < d-1:
+            text += f'- {r[k+1]} -'
+    print(text)
