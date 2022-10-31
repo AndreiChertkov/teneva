2c70df4a306b68fc52a74c49637f27ab817720e0
AndreiChertkov <andre.chertkov@gmail.com>
2022-10-31 17:53:14 +0300

    Some fixes for "interp_common.py"

--- teneva/core/interp_common.py
+++ teneva/core/interp_common.py
@@ -2,48 +2,66 @@ from functools import reduce
 
 
 def TT_to_Tucker(Y, X, H, rcond=1e-6):
-    """
-    Y - list of cores
-    X - vaues of X in each core (or the same for all)
-    H -- function, which corresponds to the values of H-matrix in TT-format and its d, 
-        returns np.array of size N x m, N -- number of points, m -- number of basis functions 
+    """Construct the TT-Tucker approximation for the given TT-tensor.
+
+    Args:
+        Y (list): TT-tensor.
+        X (list, np.ndarray): values of argument in each TT-core (2-dim array or
+            list of lists) or the same for all (1-dim array or list).
+        H (function): function, which corresponds to the values of "H-matrix"
+            in the TT-format, it returns np.ndarray of the size "n x m", where
+            n is a number of points and m is a number of basis functions.
+
+    Returns:
+        list: TT-tensor, which represents the interpolation coefficients.
+
     """
     d = len(Y)
     X = np.asarray(X)
+
     if X.ndim == 1:
-        H_mats = [H(X).T]*d
-        X = [X]*d
+        H_mats = [H(X).T] * d
+        X = [X] * d
     else:
-        H_mats = [None]*d
+        H_mats = [None] * d
 
     res = []
-    for c, X_i, H_mat in zip(Y, X, H_mats):
+    for G, X_curr, H_mat in zip(Y, X, H_mats):
         if H_mat is None:
-            H_mat = H(X_i).T
+            H_mat = H(X_curr).T
 
-        r1, n, r2 = c.shape
-        M = np.transpose(c, [1, 0, 2]).reshape(n, -1)
-        c = sp.linalg.lstsq(H_mat, M, overwrite_a=False, overwrite_b=True,  rcond=rcond)[0]
-        c = np.transpose(c.reshape(n, r1, r2), [1, 0, 2])
-        res.append(c)
+        r1, n, r2 = G.shape
+        M = np.transpose(G, [1, 0, 2]).reshape(n, -1)
+
+        Q = sp.linalg.lstsq(H_mat, M, overwrite_a=False, overwrite_b=True,
+            rcond=rcond)[0]
+        Q = np.transpose(Q.reshape(n, r1, r2), [1, 0, 2])
+        res.append(Q)
 
     return res
 
 
-def get_spectral(cores, X, H):
-    """
-    cores - list of cores
-    X - vaues of X, 1D array of size len(cores)
-    H -- function, which corresponds to the values of H-matrix in TT-format and its d, 
-        returns np.array of size N x m, N -- number of points, m -- number of basis functions 
+def get_spectral(Y, X, H):
+    """??? TODO.
+
+    Args:
+        Y (list): TT-tensor.
+        X (list, np.ndarray): values of argument in each TT-core (2-dim array or
+            list of lists) or the same for all (1-dim array or list).
+        H (function): function, which corresponds to the values of "H-matrix"
+            in the TT-format, it returns np.ndarray of the size "n x m", where
+            n is a number of points and m is a number of basis functions.
+
+    Returns:
+        ????: ??? TODO.
+
     """
     X = np.asanyarray(X)
-    assert X.ndim == 1 and len(X) == len(cores)
+    
+    assert X.ndim == 1 and len(X) == len(Y)
 
     def f(v, arg):
         c, y = arg
         return np.einsum("i,j,ijk->k", v, y[:c.shape[1]], c)
 
-    return reduce(f, zip(cores, H(X)), np.array([1]))[0]
-
-
+    return reduce(f, zip(Y, H(X)), np.array([1]))[0]
