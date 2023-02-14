35bea268e86c66e6b51042de06b573ebf28a09c1
AndreiChertkov <andre.chertkov@gmail.com>
2023-02-14 14:45:17 +0300

    (STL,DEM)[core.matrices,core.vectors] Minor stylistic changes and comments

--- teneva/core/matrices.py
+++ teneva/core/matrices.py
@@ -5,6 +5,7 @@ various useful QTT-matrices (delta function and others).
 
 """
 import numpy as np
+import teneva
 
 
 def matrix_delta(q, i, j, v=1.):
@@ -12,7 +13,7 @@ def matrix_delta(q, i, j, v=1.):
 
     Args:
         q (int): quantization level. The resulting matrix will have the shape
-            2^q x 2^q (> 0).
+            "2^q x 2^q".
         i (int): the col index for nonzero element (< 2^q). Note that "negative
             index notation" is supported.
         j (int): the row index for nonzero element (< 2^q). Note that "negative
@@ -23,10 +24,10 @@ def matrix_delta(q, i, j, v=1.):
         list: TT-tensor with 4D TT-cores representing the QTT-matrix.
 
     """
-    i = _index_prepare(q, i)
-    j = _index_prepare(q, j)
-    ind_col = _index_expand(q, i)
-    ind_row = _index_expand(q, j)
+    i = teneva._vector_index_prepare(q, i)
+    j = teneva._vector_index_prepare(q, j)
+    ind_col = teneva._vector_index_expand(q, i)
+    ind_row = teneva._vector_index_expand(q, j)
     Y = []
     for k in range(q):
         G = np.zeros((1, 2, 2, 1))
@@ -34,20 +35,3 @@ def matrix_delta(q, i, j, v=1.):
         Y.append(G)
     Y[-1][0, ind_col[-1], ind_row[-1], 0] = v
     return Y
-
-
-def _index_expand(q, i):
-    if i < 0:
-        if i == -1:
-            ind = [1] * q
-        else:
-            raise ValueError('Only "-1" is supported for negative indices.')
-    else:
-        ind = []
-        for _ in range(q):
-            ind.append(i % 2)
-            i = int(i / 2)
-        if i > 0:
-            raise ValueError('Index is out of range.')
-
-    return ind
