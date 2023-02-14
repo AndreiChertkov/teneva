377b6018ffb1b17c3c00924ead3c4c32aaebd5e8
AndreiChertkov <andre.chertkov@gmail.com>
2023-02-14 14:30:55 +0300

    (UPG,STL,DEM)[core.data.accuracy_on_data] Replace "getter" with "get_many"

--- doc/core/data.rst
+++ doc/core/data.rst
@@ -23,25 +23,25 @@ Module data: functions for working with datasets
     n = [5] * 10  # Shape of the tensor
     
     # Random TT-tensor with TT-rank 2:
-    Y = teneva.tensor_rand(n, 2)
+    Y = teneva.rand(n, 2)
     
     # Let build toy dataset:
     I_data = teneva.sample_lhs(n, m)
-    Y_data = [teneva.get(Y, i) for i in I_data]
-    Y_data = np.array(Y_data)
+    y_data = [teneva.get(Y, i) for i in I_data]
+    y_data = np.array(y_data)
     
     # Add add some noise:
-    Y_data = Y_data + 1.E-3*np.random.randn(m)
+    y_data = y_data + 1.E-3*np.random.randn(m)
     
     # Compute the accuracy:
-    eps = teneva.accuracy_on_data(Y, I_data, Y_data)
+    eps = teneva.accuracy_on_data(Y, I_data, y_data)
     
     print(f'Accuracy     : {eps:-8.2e}')
 
     # >>> ----------------------------------------
     # >>> Output:
 
-    # Accuracy     : 1.35e-04
+    # Accuracy     : 3.09e-03
     # 
 
 
--- teneva/core/data.py
+++ teneva/core/data.py
@@ -8,36 +8,32 @@ import numpy as np
 import teneva
 
 
-def accuracy_on_data(Y, I_data, Y_data, e_trunc=None):
+def accuracy_on_data(Y, I_data, y_data, e_trunc=None):
     """Compute the relative error of TT-tensor on the dataset.
 
     Args:
         I_data (np.ndarray): multi-indices for items of dataset in the form of
-            array of the shape [samples, d].
-        Y_data (np.ndarray): values for items related to I_data of dataset in
-            the form of array of the shape [samples].
-        e_trunc (float): optional truncation accuracy (> 0). If this parameter
-            is set, then sampling will be performed from the rounded TT-tensor.
+            array of the shape "[samples, d]".
+        y_data (np.ndarray): values for items related to "I_data" of dataset in
+            the form of array of the shape "[samples]".
+        e_trunc (float): optional truncation accuracy. If this parameter is
+            set, then sampling will be performed from the rounded TT-tensor.
 
     Returns:
         float: the relative error.
 
     Note:
-        If "I_data" or "Y_data" is not provided, the function will return "-1".
+        If "I_data" or "y_data" is not provided, the function will return "-1".
 
     """
-    if I_data is None or Y_data is None:
+    if I_data is None or y_data is None:
         return -1.
 
     I_data = np.asanyarray(I_data, dtype=int)
-    Y_data = np.asanyarray(Y_data, dtype=float)
+    y_data = np.asanyarray(y_data, dtype=float)
 
     if e_trunc is not None:
-        get = teneva.getter(teneva.truncate(Y, e_trunc))
-    else:
-        get = teneva.getter(Y)
+        Y = teneva.truncate(Y, e_trunc)
 
-    Z = np.array([get(i) for i in I_data])
-    e = np.linalg.norm(Z - Y_data)
-    e /= np.linalg.norm(Y_data)
-    return e
+    y = teneva.get_many(Y, I_data)
+    return np.linalg.norm(y - y_data) / np.linalg.norm(y_data)
